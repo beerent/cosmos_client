b649d3917dd86b15e651de00a19cb45dbd5af5b7
Brent Ryczak <brent.ryczak@enveyo.com>
2020-10-27 23:26:42 -0500

    fixing bug with spaces in username

--- Classes/Core/Net/RequestBuilder.cpp
+++ Classes/Core/Net/RequestBuilder.cpp
@@ -1,5 +1,7 @@
 #include <Core/Net/RequestBuilder.h>
 
+void replaceAll(std::string& str, const std::string& from, const std::string& to);
+
 RequestBuilder::RequestBuilder() {}
 
 void RequestBuilder::SetEndpoint(const std::string& endpoint) {
@@ -28,6 +30,18 @@ std::string RequestBuilder::GetRequestString() const {
 
 		requestString += (pair.first + "=" + pair.second);
 	}
+    
+    replaceAll(requestString, " ", "%20");
 
 	return requestString;
+}
+
+void replaceAll(std::string& str, const std::string& from, const std::string& to) {
+    if(from.empty())
+        return;
+    size_t start_pos = 0;
+    while((start_pos = str.find(from, start_pos)) != std::string::npos) {
+        str.replace(start_pos, from.length(), to);
+        start_pos += to.length(); // In case 'to' contains 'from', like replacing 'x' with 'yx'
+    }
 }
